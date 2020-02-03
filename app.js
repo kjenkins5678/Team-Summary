870d7fba987359b2188f026b8eeb43ed2d65faa8
kjenkins5678 <kajenkins9@gmail.com>
2020-02-03 13:54:01 -0500

    turned main inquirer prompt into one function

--- app.js
+++ app.js
@@ -1,42 +1,59 @@
-var inquirer = require("inquirer");
+const inquirer = require("inquirer");
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
-inquirer.prompt([
-    {
-        type: "input",
-        name: "name",
-        message: "Enter the Employee's Name"
-    },
-    {
-        type: "list",
-        message: "What is their Title?",
-        name: "title",
-        choices: [
-            "Manager",
-            "Engineer",
-            "Intern"
-        ]
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Enter their unique id"
-    }
-    ]).then(function(data) {
-
-        if(data.title === "Manager"){
-            console.log("Manager");
-        } else if (data.title === "Engineer"){
-            console.log("Engineer")
-        } else {
-            console.log("Intern")
+function ThePrompt (){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the Employee's Name"
+        },
+        {
+            type: "list",
+            message: "What is their Title?",
+            name: "title",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern"
+            ]
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter their unique id"
+        },
+        {
+            type: "confirm",
+            name: "continue",
+            message: "Do you want to enter more employees?"
         }
+        ]).then(function(data) {
+    
+            if(data.title === "Manager"){
+                // console.log("Manager");
+            } else if (data.title === "Engineer"){
+                // console.log("Engineer")
+            } else {
+                // console.log("Intern")
+            }
 
-    // theManager = new Manager(data.name, data.title, data.id);
-    // console.log(theManager);
-    // console.log(theManager.getRole());
+            console.log(data.continue);
+            keepGoing = data.continue;
+            
+    });
+};
 
-});
+ThePrompt();
+
+// if (keepGoing == true){
+//     ThePrompt();
+// } else {
+//     console.log("Done");
+// }
+
+
+
 
