e3d9e9e85a0a54a43d54483235c59ad4da4c2c2c
kjenkins5678 <kajenkins9@gmail.com>
2020-02-03 17:21:42 -0500

    going nowhere with all these changes

--- app.js
+++ app.js
@@ -3,57 +3,48 @@ const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
-function ThePrompt (){
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Enter the Employee's Name"
-        },
-        {
-            type: "list",
-            message: "What is their Title?",
-            name: "title",
-            choices: [
-                "Manager",
-                "Engineer",
-                "Intern"
-            ]
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Enter their unique id"
-        },
-        {
-            type: "confirm",
-            name: "continue",
-            message: "Do you want to enter more employees?"
-        }
-        ]).then(function(data) {
-    
-            if(data.title === "Manager"){
-                // console.log("Manager");
-            } else if (data.title === "Engineer"){
-                // console.log("Engineer")
-            } else {
-                // console.log("Intern")
-            }
+var output = [];
 
-            console.log(data.continue);
-            keepGoing = data.continue;
-            
-    });
-};
+var questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Enter the Employee's Name"
+    },
+    {
+        type: "list",
+        message: "What is their Title?",
+        name: "title",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Enter their unique id"
+    },
+  {
+    type: 'confirm',
+    name: 'askAgain',
+    message: 'Want to enter another employee? (just hit enter for YES)?',
+    default: true
+  }
+];
 
-ThePrompt();
+function ask() {
+  inquirer.prompt(questions).then(answers => {
+    if (answers.title == "Manager") {
+        console.log("Manager");
+    }
+    if (answers.askAgain) {
+      ask();
+    } else {
+    //   console.log('Your favorite TV Shows:', output.join(', '));
+    }
+  });
+}
 
-// if (keepGoing == true){
-//     ThePrompt();
-// } else {
-//     console.log("Done");
-// }
-
-
-
-
+ask();
