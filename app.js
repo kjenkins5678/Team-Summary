0b82c6de5a9e80afd270ef2b94421b95aca225ab
kjenkins5678 <kajenkins9@gmail.com>
2020-02-03 09:52:29 -0500

    simplifying to only one inquirer prompt so I can iterate it

--- app.js
+++ app.js
@@ -4,56 +4,39 @@ const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
 inquirer.prompt([
-{
-    type: "input",
-    name: "name",
-    message: "Enter the Manager's Name"
-},
-{
-    type: "input",
-    name: "title",
-    message: "Enter their Title"
-},
-{
-    type: "input",
-    name: "id",
-    message: "Enter their unique id"
-}
-]).then(function(data) {
+    {
+        type: "input",
+        name: "name",
+        message: "Enter the Employee's Name"
+    },
+    {
+        type: "list",
+        message: "What is their Title?",
+        name: "title",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Enter their unique id"
+    }
+    ]).then(function(data) {
 
-    theManager = new Manager(data.name, data.title, data.id);
-    console.log(theManager);
-    console.log(theManager.getRole());
-
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Enter the Employee's Name"
-        },
-        {
-            type: "list",
-            message: "What is their Title?",
-            name: "Title",
-            choices: [
-              "Engineer",
-              "Intern"
-            ]
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Enter their unique id"
+        if(data.title === "Manager"){
+            console.log("Manager");
+        } else if (data.title === "Engineer"){
+            console.log("Engineer")
+        } else {
+            console.log("Intern")
         }
-        ]).then(function(data) {
-    
-            if(data.title === "Engineer"){
-                console.log("Engineer");
-            } else {
-                console.log("Intern")
-            }
-        
-        });
+
+    // theManager = new Manager(data.name, data.title, data.id);
+    // console.log(theManager);
+    // console.log(theManager.getRole());
 
 });
 
